353f7db71edb8af664b208a8c00642d4f5d6e3d4
moknows-sysadmin <moknows-sysadmin@moknowscloudintegration.com>
2026-01-08 01:42:29 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '@/App.css';
+import ScrollToTop from '@/components/ScrollToTop';
 import HomePage from '@/pages/HomePage';
 import MedicareWaitlist from '@/pages/MedicareWaitlist';
 import VAWaitlist from '@/pages/VAWaitlist';
@@ -17,6 +18,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/medicare-waitlist" element={<MedicareWaitlist />} />
